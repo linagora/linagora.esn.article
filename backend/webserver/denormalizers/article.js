b7980f2b82298891db18099001070e39c0333736
Christophe Hamerling <chamerling@linagora.com>
2016-11-17 15:23:10 +0100

    SOC-6 Add reading time

--- backend/webserver/denormalizers/article.js
+++ backend/webserver/denormalizers/article.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const readingTime = require('reading-time');
+
 module.exports = function() {
   return function(article) {
     if (typeof article.toObject === 'function') {
@@ -7,6 +9,13 @@ module.exports = function() {
     }
     // needed for activity_streams
     article.writable = true;
+
+    const reading = readingTime(article.content);
+
+    reading.minutes = Math.ceil(reading.minutes.toFixed(2));
+    article.stats = {
+      reading
+    };
 
     return Promise.resolve(article);
   };
