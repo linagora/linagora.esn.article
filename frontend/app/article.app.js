a688efa4fc8ada12cb9f6eb45a20ed66c4768b63
Christophe Hamerling <chamerling@linagora.com>
2016-11-17 15:04:38 +0100

    SOC-6 Article header

--- frontend/app/article.app.js
+++ frontend/app/article.app.js
@@ -4,9 +4,11 @@
   angular.module('linagora.esn.article', [
     'angularMoment',
     'esn.core',
+    'esn.follow',
     'esn.header',
     'esn.router',
     'esn.sidebar',
+    'esn.user',
     'restangular',
     'esn.lodash-wrapper',
     'op.dynamicDirective',
