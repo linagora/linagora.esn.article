9a751ee2bc6f0e06723e4350ce35966a610ffc13
Christophe Hamerling <chamerling@linagora.com>
2016-11-29 12:23:25 +0100

    SOC-14 Add closed badge

--- frontend/app/article/list/article-list-item.controller.js
+++ frontend/app/article/list/article-list-item.controller.js
@@ -4,9 +4,10 @@
   angular.module('linagora.esn.article')
     .controller('articleListItemController', articleListItemController);
 
-   function articleListItemController(ARTICLE_OBJECT_TYPE) {
+   function articleListItemController(ARTICLE_OBJECT_TYPE, ARTICLE_STATUS) {
      var self = this;
 
      self.ARTICLE_OBJECT_TYPE = ARTICLE_OBJECT_TYPE;
+     self.ARTICLE_STATUS = ARTICLE_STATUS;
    }
 })();
